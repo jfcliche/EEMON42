34e168a1337252feebd1aa3428d15cf50b326019
jfcliche[P5K] <jfcliche@jfcliche.com>
2023-03-29 21:59:17 -0400

    button: implemented IRQ-baes basic debouncing. not perfect. Removed unused code.

--- software/button.py
+++ software/button.py
@@ -1,53 +1,28 @@
 # Debounced Button
-# Ref: https://gist.github.com/SpotlightKid/0a0aac56606286a80f860b116423c94f
 
-# inspired by: https://forum.micropython.org/viewtopic.php?t=1938#p10931
-
-import micropython
-
-try:
-    from machine import Timer
-    def timer_init(t, p, cb): return t.init(period=p, callback=cb)
-except ImportError:
-    from pyb import Timer
-    def timer_init(t, p, cb): return t.init(freq=1000 // p, callback=cb)
-
+import time  # using functions not compatible with Cpython
 
 class Button:
-    def __init__(self, sw, delay=50, tid=4, irq_wrapper=None):
+    def __init__(self, sw, delay=50, irq_wrapper=None):
         self.sw = sw
-        # Create references to bound methods beforehand
-        # http://docs.micropython.org/en/latest/pyboard/library/micropython.html#micropython.schedule
-        self._sw_cb = self._sw_cb
-        self._tim_cb = self._tim_cb
-        self._set_cb = getattr(self.sw, 'callback', None) or self.sw.irq
-        self._value = 0
         self._delay = delay
-        self._tim = Timer(tid)
-        # self._set_callback(self._callback, None)
+        self.last_time_0 = 0
+        self.last_time_1 = 0
+        self._value = 0
         if irq_wrapper:
-            self.sw.irq(irq_wrapper(self._callback))
+            self.sw.irq(irq_wrapper(self.process_irq))
         else:
-            self.sw.irq(self._callback)
-    def _sw_cb(self, pin=None):
-        self._set_cb(None)
-        timer_init(self._tim, self._delay, self._tim_cb)
+            self.sw.irq(self.process_irq)
 
-    def _tim_cb(self, tim):
-        tim.deinit()
-        if self.sw():
-            micropython.schedule(self.cb, self.arg)
-        self._set_cb(self._sw_cb if self.cb else None)
-
-    def _set_callback(self, cb, arg=None):
-        self._tim.deinit()
-        self.cb = cb
-        self.arg = arg
-        self._set_cb(self._sw_cb if cb else None)
-
-    def _callback(self, a):
-        self._value += 1
-        print(f'Button {a} value={self._value}')
+    def process_irq(self, pin):
+        t = time.ticks_ms()
+        if not pin.value(): # 0 when button is pressed
+            if time.ticks_diff(t, self.last_time_1) > self._delay and time.ticks_diff(t, self.last_time_0) > (self._delay << 1):
+                self._value += 1
+                print(f'Button {pin}={pin.value()} value={self._value}, dt0={time.ticks_diff(t, self.last_time_0)} dt1={time.ticks_diff(t, self.last_time_1)}')
+            self.last_time_0 = t
+        else:
+            self.last_time_1 = t            
 
     def value(self):
         ret_value = self._value
