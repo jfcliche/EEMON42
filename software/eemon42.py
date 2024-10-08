94a10f7619a26b9caebf36a4148ee85a8e6e190b
jfcliche[P5K] <jfcliche@jfcliche.com>
2024-10-08 12:23:00 -0400

    feat (eemon42): Added pin interrupt on IRQ with ThreadSafeflag,  added screen saver, improve emon readout loop.

--- software/eemon42.py
+++ software/eemon42.py
@@ -12,8 +12,10 @@ import json
 import sys
 if sys.implementation.name == 'micropython':
     import uasyncio as asyncio
+    from uasyncio import ThreadSafeFlag 
 else:
     import asyncio
+    from asyncio import Event as ThreadSafeFlag 
 
 # local packages
 from ssd1331 import SSD1331  # OLED display
@@ -49,6 +51,13 @@ class EEMON42:
 
         self.message_interval = 5
 
+
+        print('Welcome to EEMON42')
+        print('   Creating EEMON42 instance')
+
+        self.irq_flag = ThreadSafeFlag()
+        # Pin definitions
+        # Pin numbers are GPIO numbers, not package pins numbers
         self.pin_sck = Pin(6, Pin.OUT)  # SPI clock
         self.pin_mosi = Pin(7, Pin.OUT) # SPI MOSI
         self.pin_miso = Pin(2, Pin.IN)  # SPI MISO
@@ -56,17 +65,18 @@ class EEMON42:
         self.pin_cd = Pin(1, Pin.OUT) # SPI Display Command/Data line
         self.pin_res = Pin(0, Pin.OUT) # SPI display reset
         # The following pins are dual functions (EMON chip-select and switches or IRQ).
-        # Teir mode is managed by the SPI module
+        # Their mode is managed by the SPI module
         self.pin_cs0_rota = Pin(10)
         self.pin_cs1_rotb = Pin(9)
-        self.pin_cs2_button_rot = Pin(8)
-        self.pin_cs3_button_a = Pin(3)
-        self.pin_cs4_button_b = Pin(5)
-        self.pin_cs5_button_c = Pin(4)
+        self.pin_cs2_button_rot = Pin(8) # Rotary encoder pushbutton switch
+        self.pin_cs3_button_a = Pin(3) # SW2, bottom switch, usually ENTER
+        self.pin_cs4_button_b = Pin(5) # SW3, top switch, usually ESC/BACK
+        self.pin_cs5_button_c = Pin(4) # SW4, middle switch, usually SHIFT
         self.pin_cs6_irq = Pin(21)
 
-        # List of pins that are used *both* for buttton/encoder/irq inputs and energy monitor chip select outputs
-        dual_fn_pins = (self.pin_cs0_rota, 
+        # List of pins that are used as ADE7816 chip select outputs. 
+        # Those are *ALSO* used for buttton/encoder/irq inputs
+        emon_cs_pins = (self.pin_cs0_rota, 
                         self.pin_cs1_rotb,
                         self.pin_cs2_button_rot,
                         self.pin_cs3_button_a,
@@ -74,12 +84,13 @@ class EEMON42:
                         self.pin_cs5_button_c,
                         self.pin_cs6_irq)
 
+        # Instantiate the SPI controller
         self.spi = SPI_with_CS(
                 baudrate=2500000,  # 2.5 MHz clock 
                 sck=self.pin_sck,
                 mosi=self.pin_mosi,
                 miso=self.pin_miso,
-                cs_inout_pins=dual_fn_pins  # these pins are set to mode=Pin.OUT during SPI transactions to prevent button operations to enable CS lines 
+                cs_inout_pins=emon_cs_pins  # these pins are set to mode=Pin.OUT during SPI transactions to prevent button operations to enable CS lines 
                 )
 
         # Display handler
@@ -87,48 +98,60 @@ class EEMON42:
 
         self.counter = 0
 
-        # Rotary Encoder handler
-        self.rot_enc = RotaryEncoder(
-            pin_a=self.pin_cs0_rota, 
-            pin_b=self.pin_cs1_rotb, 
-            irq_wrapper=self.spi.get_irq, # provide a IRQ handler that is disabled during SPI transactions
-            verbose=0)
-
         # Button handlers
         self.button_rot = Button(self.pin_cs2_button_rot, irq_wrapper=self.spi.get_irq)
         self.button_a = Button(self.pin_cs3_button_a, irq_wrapper=self.spi.get_irq)
         self.button_b = Button(self.pin_cs4_button_b, irq_wrapper=self.spi.get_irq)
         self.button_c = Button(self.pin_cs5_button_c, irq_wrapper=self.spi.get_irq)
 
+        # Rotary Encoder handler
+        self.rot_enc = RotaryEncoder(
+            pin_a=self.pin_cs0_rota, 
+            pin_b=self.pin_cs1_rotb,
+            button_shift = self.button_c, 
+            irq_wrapper=self.spi.get_irq, # provide a IRQ handler that is disabled during SPI transactions
+            verbose=0)
+
         # Create the 7 energy monitor handlers
         self.emon = [ADE7816(spi=self.spi, cs_pin=cs_pin, irq_pin=self.pin_cs6_irq, irq_wrapper=self.spi.get_irq, index=ix) 
-                     for ix, cs_pin in enumerate(dual_fn_pins)]
+                     for ix, cs_pin in enumerate(emon_cs_pins)]
 
+        # Setup the ADE7816 IRQ line interrupt handler
         self.pin_cs6_irq.irq(handler=self.spi.get_irq(self.emon_irq_handler));
          
 
 
         # Create the GUI (display + buttons, menu system etc) handler
-        self.gui = GUI(self.display, self.rot_enc, self.button_rot, self.button_a, self.button_b, self.button_c)
+        self.gui = GUI(self.display, self.rot_enc, self.button_rot, button_enter=self.button_a, button_esc=self.button_b, button_shift=self.button_c)
 
 
-        print('Loading configuration file')
+        print('   Loading configuration file')
         if not self.load_config():
-            print("Unable to load the configuration")
-            return
+            raise RuntimeError("Unable to load the configuration file")
+        print('   Instantiation complete')
+
 
     def emon_irq_handler(self, pin):
-        print(f'Got pin IRQ!')
-        if False:
-            for e in self.emon:
-                e.irq_handler()
+        if not pin.value():
+            self.irq_flag.set()
+        # pass
 
     def init(self):
+        """ Initialize hardware
+
+        This function toggles bits and communicates with the external hardware to bring it into known conditions, as opposed to ``__init__``
+        which only created the hardware objects.
+        """
+        # Initialize display
+        self.display.init()
         self.display.print("EEMON42\r\nis\r\nthe\r\nbest\nof\nall", fg=self.display.YELLOW, font_size=5)
+
+        # Initialize Energy Monitor ICs
+
         for e in self.emon:
             e.init()
-        time.sleep(1)
-        self.display.clear()
+        # time.sleep(1)
+        # self.display.clear()
 
 
 
@@ -234,14 +257,22 @@ class EEMON42:
         self.spi = None
 
     async def screen_saver(self, timeout=10):
-        while True:
-            t = time.time() - self.rot_enc.last_time - timeout
-            if t > 0:
-                self.display.clear()
-            # elif t > 0:
-            #     self.display.set_master_intensity(0)
-            await asyncio.sleep(1)
-
+        try:
+            while True:
+                dt = int(time.time() - self.display.last_time) 
+                if dt > 2*timeout:
+                    self.display.set_brightness(0) # turn off display
+                elif dt > timeout:
+                    # print(f'Setting brightness to {1} {dt=}')
+                    self.display.set_brightness(1) # set minimum brightness
+                    # # self.display.clear()
+                    # b = max(0, int(16 - dt))
+                    # self.display.set_brightness(b)
+                # elif t > 0:
+                #     self.display.set_master_intensity(0)
+                await asyncio.sleep(1)
+        except Exception as e:
+            print(f'Exception in screen_saver: {e}')
 
     async def watchdog(self): 
         """ Monitors the system status flags and reset the board if a fatal error is detected
@@ -252,25 +283,36 @@ class EEMON42:
             await asyncio.sleep(1)
 
     async def scan_emon(self):
-        ch = 0;
-        ch_max = len(self.emon) - 1
+        dev = 0  # current energy monitor device number
+        n_dev = len(self.emon) # total number of energy monitor devices
+
+        # define local variables for faster access
+        emon = self.emon
+        irq_pin = self.pin_cs6_irq  
+        irq_flag = self.irq_flag
+        spi = self.spi
+
         while True:
             try:
-                # for ix, e in enumerate(self.emon):
-                #     f = e.get_frequency()
-                #     print(f'CH{ix} = {f} Hz')
+                # If there is not already an IRQ, wait for the IRQ flag to be set by the pin interrupt, 
+                # but continue anyways after a timeout in case the IRQ pin went low without the interrupt being processed
+                if irq_pin():
+                    try:
+                        await asyncio.wait_for(irq_flag.wait(), 13)
+                        irq_flag.clear()
+                    except asyncio.TimeoutError:
+                        print(f'Timout while waiting for IRQ, {dev=}, IRQ={irq_pin()}')
 
-                #     for ch in range(1):
-                #         i = e.get_rms_current(ch)
-                #         print(f'CH{ix}.{ch} = {i:.3f} A RMS')
-                #         if not ch:
-                #             self.display.print(f'CH{ix} = {i:.3f} A RMS', x=0, y=ix*self.display.font_height)
-                while not self.pin_cs6_irq():
-                    if self.emon[ch].irq_handler(self.pin_cs6_irq):
-                        ch = ch + 1 if ch < ch_max else 0
-                    await asyncio.sleep(0) 
-                print('.', end='')
-                await asyncio.sleep(0.1)
+                scanned_dev = 0
+                while not irq_pin() and not spi.spi_active and scanned_dev < n_dev:
+                    emon[dev].irq_handler(irq_pin)
+                    dev += 1
+                    if dev >= n_dev: 
+                        dev = 0
+                    scanned_dev += 1
+                    await asyncio.sleep(0)  # be a good neighbor and give back control to the event loop to let the UI respond to user actions 
+                # print('---')
+                # await asyncio.sleep(0.1)
             except Exception as e:
                 print(f'scan_emon exception {e}')
                 raise
@@ -280,8 +322,6 @@ class EEMON42:
 
         print('Starting main loop')
 
-
- 
         # Start background tasks
         print('Starting background tasks')
         task_list = (
@@ -306,10 +346,6 @@ class EEMON42:
         print('Exiting main loop')
  
     def run(self):
-        # init()
-        # start_gui()
-        # start_client()
-
         try:
             asyncio.run(self.main_loop())
         except KeyboardInterrupt:
