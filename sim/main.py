2410d6ff9ff118ee304528c389b55d2bb939ae76
jfcliche[P5K] <jfcliche@jfcliche.com>
2024-10-08 12:29:35 -0400

    feat (sim): parametrize graphics. Remove init() from __init__().

--- sim/main.py
+++ sim/main.py
@@ -25,7 +25,52 @@ from ssd1331 import SSD1331
 
 class EEMON42Sim(EEMON42):
 
-    zoom_display = False
+    DISPLAY_POS = (180, 66)  # Upper left corner of the display area
+    BUTTON_ROT_RECT = (169, 165, 58, 47)
+    BUTTON_B_RECT = (246, 161, 26, 28)
+    BUTTON_C_RECT = (246, 203, 26, 28)
+    BUTTON_A_RECT = (246, 245, 26, 28)
+    BUTTON_ZOOM_RECT = (DISPLAY_POS[0] - 6, DISPLAY_POS[1] - 6, SSD1331.WIDTH + 12, SSD1331.HEIGHT + 12) 
+
+    def __init__(self):
+        pygame.init()
+
+        self.zoom_display = False
+        self.board = pygame.image.load("eemon42.png")
+        pygame.display.set_caption("EEMON42 Simulator")
+        self.screen = pygame.display.set_mode(self.board.get_size())
+        # SSD1331.surface = self.surface = pygame.surface.Surface((SSD1331.WIDTH, SSD1331.HEIGHT))
+
+
+        # Initialize the original EEMON42
+        super().__init__()
+
+        # Pass the surface object to the emulated display instance. 
+        # self.display.set_surface(self.surface)  
+
+
+        # Pass graphical parameters to the emulated button instances
+        self.button_rot.set_rect(pygame.Rect(*self.BUTTON_ROT_RECT))
+        self.button_a.set_rect(pygame.Rect(*self.BUTTON_A_RECT))
+        self.button_b.set_rect(pygame.Rect(*self.BUTTON_B_RECT))
+        self.button_c.set_rect(pygame.Rect(*self.BUTTON_C_RECT))
+
+        # add a display zoom button
+        self.zoom_button = Button(rect=pygame.Rect(*self.BUTTON_ZOOM_RECT))
+
+        # create a list of graphical widgets to process 
+        self.widgets = (self.rot_enc,
+                        self.button_rot,
+                        self.button_a,
+                        self.button_b,
+                        self.button_c,
+                        self.zoom_button
+                        )
+
+        print('Controls:')
+        print('  Use the wheel anywhere to rotate the knob')
+        print('  Click on the buttons or knob to press them')
+        print('  Clock on the screen to show/hide a magnified display')
 
     async def pygame_event_loop(self):
         while True:
@@ -45,12 +90,12 @@ class EEMON42Sim(EEMON42):
 
             # Display screen
             self.screen.blit(self.board, self.screen.get_rect())
-            self.screen.blit(self.surface, (180, 66))
+            self.screen.blit(self.display.surface, self.DISPLAY_POS)
 
             # Display screen zoom insert, if activated
             if self.zoom_display:
                 self.screen.blit(pygame.transform.scale(
-                    self.surface, (SSD1331.WIDTH * 2, SSD1331.HEIGHT * 2)), (0, self.screen.get_rect().bottom-SSD1331.HEIGHT * 2))
+                    self.display.surface, (SSD1331.WIDTH * 2, SSD1331.HEIGHT * 2)), (0, self.screen.get_rect().bottom-SSD1331.HEIGHT * 2))
 
             # Display widget bounding boxes
             for widget in self.widgets:
@@ -60,52 +105,13 @@ class EEMON42Sim(EEMON42):
             await asyncio.sleep(0.03)
         print('pygame event processing loop has exited')
 
-    def __init__(self):
-        pygame.init()
-        self.board = pygame.image.load("eemon42.png")
-        pygame.display.set_caption("EEMON42 Simulator")
-        self.screen = pygame.display.set_mode(self.board.get_size())
-        SSD1331.surface = self.surface = pygame.surface.Surface((SSD1331.WIDTH, SSD1331.HEIGHT))
-
-
-        # Initialize the original EEMON42
-        super().__init__()
-
-        # Pass the surface object to the emulated display instance. 
-        self.display.set_surface(self.surface)  
-
-        # initializes the simulated hardware
-        super().init()
-
-        # Pass graphical parameters to the emulated button instances
-        self.button_rot.set_rect(pygame.Rect(169, 165, 58, 47))
-        self.button_a.set_rect(pygame.Rect(246, 161, 26, 28))
-        self.button_b.set_rect(pygame.Rect(246, 203, 26, 28))
-        self.button_c.set_rect(pygame.Rect(246, 245, 26, 28))
-
-        # add a display zoom button
-        self.zoom_button = Button(rect=pygame.Rect(180-6, 66-6, 96 + 12, 64 + 12))
-
-        # create a list of graphical widgets to process 
-        self.widgets = (self.rot_enc,
-                        self.button_rot,
-                        self.button_a,
-                        self.button_b,
-                        self.button_c,
-                        self.zoom_button
-                        )
-
-        print('Controls:')
-        print('  Use the wheel anywhere to rotate the knob')
-        print('  Click on the buttons or knob to press them')
-        print('  Clock on the screen to show/hide a magnified display')
-
     async def main_loop(self):
         """ Runs the main program loop of the EEMON42, with an additional pygame display handling task.
         """
         self.pygame_task = asyncio.create_task(self.pygame_event_loop())
-        self.main_task = asyncio.create_task(super().main_loop())  # start main loop as an explicit task so we can cancel it.
-        await self.main_task # Run until the main loop exits for some reason (return or exception)
+        # self.main_task = asyncio.create_task(super().main_loop())  # start main loop as an explicit task so we can cancel it.
+        # await self.main_task # Run until the main loop exits for some reason (return or exception)
+        await super().main_loop()  # start main loop as an explicit task so we can cancel it.
 
     def run(self):
         try:
@@ -114,6 +120,10 @@ class EEMON42Sim(EEMON42):
             print('Cancelled by user. Bye!')
         except KeyboardInterrupt:
             print('Interrupted by user. Bye!')
+        except Exception as e:
+            print('Exception!')
+            raise
+
 
 if __name__=="__main__":
     sim = EEMON42Sim()
